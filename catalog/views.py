50ec4949fff179112b5657740331a78f3c034a79
Dmitry Dronov <cskafan1911@gmail.com>
2023-11-25 12:00:59 +0300

    Создан контроллер и шаблон product_info. Выделен базовый шаблон base.html.

--- catalog/views.py
+++ catalog/views.py
@@ -1,14 +1,46 @@
 from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
+
+from catalog.models import Products, Category
 
 
-def home(request):
-    return render(request, 'catalog/home.html')
+def index(request):
+    products_list = Products.objects.all()
+    context = {
+        'object_list': products_list,
+        'title': 'Товары для спорта',
+    }
+
+    return render(request, 'catalog/index.html', context)
 
 
-def contacts(request):
+def product_info(request, pk):
+    context = {
+        'object': Products.objects.get(pk=pk),
+    }
+
+    return render(request, 'catalog/product_info.html', context)
+
+
+def product_add(request):
+
     if request.method == 'POST':
-        name = request.POST.get('name')
-        phone = request.POST.get('phone')
-        message = request.POST.get('message')
-        print(f"{name} ({phone}) Оставил сообщение: {message}")
-    return render(request, 'catalog/contacts.html')
+        new_product = {
+            'name': request.POST.get('product_name'),
+            'description': request.POST.get('description'),
+            'image': request.POST.get('image'),
+            'price': request.POST.get('price'),
+            'category': Category.objects.get(pk=request.POST.get('category'))
+        }
+        print(new_product['image'])
+        Products.objects.create(**new_product)
+
+    return render(request, 'catalog/product_add.html')
+
+    # def contacts(request):
+    #     if request.method == 'POST':
+    #         name = request.POST.get('name')
+    #         phone = request.POST.get('phone')
+    #         message = request.POST.get('message')
+    #         print(f"{name} ({phone}) Оставил сообщение: {message}")
+    #     return render(request, 'catalog/contacts.html')
