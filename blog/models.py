49696b463be4776ec3781513742b2e7b85a00aca
Dmitry Dronov <cskafan1911@gmail.com>
2023-12-02 18:16:40 +0300

    Изменения в модели Blog

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,7 @@ NULLABLE = {'blank': True, 'null': True}
 
 class Blog(models.Model):
     title = models.CharField(max_length=50, verbose_name='Заголовок')
-    slug = models.CharField(max_length=100, verbose_name='Slug', **NULLABLE)
+    slug = models.CharField(max_length=100, unique=True, verbose_name='Slug', **NULLABLE)
     content = models.TextField(verbose_name='Содержимое')
     preview = models.ImageField(upload_to='preview_image', **NULLABLE, verbose_name='Превью')
     date_of_creation = models.DateTimeField(**NULLABLE, verbose_name='Дата создания')
