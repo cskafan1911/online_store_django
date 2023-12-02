0f827b56d5535dafd79dbc4010d1968f4f2c1bc7
Dmitry Dronov <cskafan1911@gmail.com>
2023-12-02 13:33:20 +0300

    Изменения urls приложения Blog.

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
 from blog.apps import BlogConfig
-from blog.views import BlogCreateView
+from blog.views import BlogCreateView, BlogListView, BlogDetailView, BlogUpdateView, BlogDeleteView
 
 app_name = BlogConfig.name
 
 urlpatterns = [
+    path('', BlogListView.as_view(), name='blogs'),
     path('create/', BlogCreateView.as_view(), name='blog_create'),
+    path('info/<int:pk>/', BlogDetailView.as_view(), name='blog_info'),
+    path('update/<int:pk>/', BlogUpdateView.as_view(), name='blog_update'),
+    path('delete/<int:pk>/', BlogDeleteView.as_view(), name='blog_delete'),
 ]
