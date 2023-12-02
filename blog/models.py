f53fbdda07f796e92ea4512c55d5473baba1a412
Dmitry Dronov <cskafan1911@gmail.com>
2023-12-02 10:07:58 +0300

    Создана модель Blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=50, verbose_name='Заголовок')
+    slug = models.CharField(max_length=100, verbose_name='Slug')
+    content = models.TextField(verbose_name='Содержимое')
+    preview = models.ImageField(upload_to='preview_image', **NULLABLE, verbose_name='Превью')
+    date_of_creation = models.DateTimeField(**NULLABLE, verbose_name='Дата создания')
+
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Блог'
+        verbose_name_plural = 'Блоги'
