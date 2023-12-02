c89576dfc353e23186a63fbe18c20a6344834449
Dmitry Dronov <cskafan1911@gmail.com>
2023-12-02 13:31:35 +0300

    Изменения в модели Blog, сделана миграция.

--- blog/models.py
+++ blog/models.py
@@ -5,12 +5,13 @@ NULLABLE = {'blank': True, 'null': True}
 
 
 class Blog(models.Model):
-    title = models.CharField(max_length=50, verbose_name='Название')
-    slug = models.CharField(max_length=100, verbose_name='Slug')
+    title = models.CharField(max_length=50, verbose_name='Заголовок')
+    slug = models.CharField(max_length=100, verbose_name='Slug', **NULLABLE)
     content = models.TextField(verbose_name='Содержимое')
     preview = models.ImageField(upload_to='preview_image', **NULLABLE, verbose_name='Превью')
     date_of_creation = models.DateTimeField(**NULLABLE, verbose_name='Дата создания')
-    publication_status = models.BooleanField
+    view_count = models.IntegerField(default=0, verbose_name='Количество просмотров')
+    is_published = models.BooleanField(default=True, verbose_name='Признак публикации')
 
     def __str__(self):
         return self.title
