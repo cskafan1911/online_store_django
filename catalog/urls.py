5280fabdddd2e3792834b252bf13be4ce4406061
Dmitry Dronov <cskafan1911@gmail.com>
2023-12-02 13:32:59 +0300

    Изменения в шаблонах и urls приложения Catalog.

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from catalog.apps import CatalogConfig
 from catalog.views import ProductsCreateView, CategoryListView, ProductsListView, IndexView, ProductsUpdateView, \
-    ProductInfoView, ProductsDeleteView
+    ProductsDeleteView, ProductsDetailView
 
 app_name = CatalogConfig.name
 
@@ -11,7 +11,7 @@ urlpatterns = [
     path('categories/', CategoryListView.as_view(), name='categories'),
     path('category_products/<int:pk>/', ProductsListView.as_view(), name='category_products'),
     path('products/<int:pk>/', ProductsListView.as_view(), name='product'),
-    path('products/<int:pk>/info/', ProductInfoView.as_view(), name='product_info'),
+    path('products/<int:pk>/info/', ProductsDetailView.as_view(), name='product_info'),
     path('products/create/', ProductsCreateView.as_view(), name='product_create'),
     path('products/update/<int:pk>/', ProductsUpdateView.as_view(), name='product_update'),
     path('products/delete/<int:pk>/', ProductsDeleteView.as_view(), name='product_delete'),
