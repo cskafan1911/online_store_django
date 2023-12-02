6499bf14e12f03bcb316f0d8d2131e51cc1664a7
Dmitry Dronov <cskafan1911@gmail.com>
2023-12-02 14:17:00 +0300

    Реализован slug записей.

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,44 @@
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
+from pytils.translit import slugify
 
 from blog.models import Blog
 
 
 class BlogCreateView(CreateView):
     model = Blog
-    fields = ('title', 'content', 'date_of_creation')
+    fields = ('title', 'content', 'preview', 'date_of_creation', 'is_published')
     success_url = reverse_lazy('blog:blogs')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_blog = form.save()
+            new_blog.slug = slugify(new_blog.title)
+            new_blog.save()
+
+        return super().form_valid(form)
 
 
 class BlogUpdateView(UpdateView):
     model = Blog
-    fields = ('title', 'content', 'date_of_creation')
-    success_url = reverse_lazy('blog:blogs')
+    fields = ('title', 'content', 'preview', 'date_of_creation', 'is_published')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_blog = form.save()
+            new_blog.slug = slugify(new_blog.title)
+            new_blog.save()
+
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('blog:blog_info', args=[self.kwargs.get('pk')])
+
 
 
 class BlogDeleteView(DeleteView):
     model = Blog
-    fields = ('title', 'content', 'date_of_creation')
+    fields = ('title', 'content', 'preview', 'date_of_creation', 'is_published')
     success_url = reverse_lazy('blog:blogs')
 
 
@@ -27,6 +47,12 @@ class BlogListView(ListView):
     extra_context = {
         'title': 'Блоги'
     }
+
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        queryset = queryset.filter(is_published=True)
+
+        return queryset
 
 
 class BlogDetailView(DetailView):
