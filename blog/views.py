ce80c0b963cce004e14017b44dba564850146a84
Dmitry Dronov <cskafan1911@gmail.com>
2023-12-02 13:29:43 +0300

    Написаны контроллеры BlogUpdateView, BlogDeleteView, BlogDetailView, BlogListView

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 
 from blog.models import Blog
 
@@ -8,5 +7,27 @@ from blog.models import Blog
 class BlogCreateView(CreateView):
     model = Blog
     fields = ('title', 'content', 'date_of_creation')
-    success_url = reverse_lazy('blog:')
+    success_url = reverse_lazy('blog:blogs')
 
+
+class BlogUpdateView(UpdateView):
+    model = Blog
+    fields = ('title', 'content', 'date_of_creation')
+    success_url = reverse_lazy('blog:blogs')
+
+
+class BlogDeleteView(DeleteView):
+    model = Blog
+    fields = ('title', 'content', 'date_of_creation')
+    success_url = reverse_lazy('blog:blogs')
+
+
+class BlogListView(ListView):
+    model = Blog
+    extra_context = {
+        'title': 'Блоги'
+    }
+
+
+class BlogDetailView(DetailView):
+    model = Blog
