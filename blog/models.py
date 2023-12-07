a5b8a2ad6988b68500d888d3ed921eff46c7fdc0
Dmitry Dronov <cskafan1911@gmail.com>
2023-12-07 20:17:10 +0300

    В модели Blog в поле date_of_creation добавлен атрибут auto_now_add=True.

--- blog/models.py
+++ blog/models.py
@@ -9,7 +9,7 @@ class Blog(models.Model):
     slug = models.CharField(max_length=100, unique=True, verbose_name='Slug', **NULLABLE)
     content = models.TextField(verbose_name='Содержимое')
     preview = models.ImageField(upload_to='preview_image', **NULLABLE, verbose_name='Превью')
-    date_of_creation = models.DateTimeField(**NULLABLE, verbose_name='Дата создания')
+    date_of_creation = models.DateTimeField(auto_now_add=True, **NULLABLE, verbose_name='Дата создания')
     view_count = models.IntegerField(default=0, verbose_name='Количество просмотров')
     is_published = models.BooleanField(default=True, verbose_name='Признак публикации')
 
