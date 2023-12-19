185f4cc878db84b476c837952ab4bb97b9892c7e
Dmitry Dronov <cskafan1911@gmail.com>
2023-12-19 23:51:43 +0300

    В ProductsForm правки в clean-методы: clean_name, clean_description

--- catalog/forms.py
+++ catalog/forms.py
@@ -4,7 +4,24 @@ from catalog.models import Products
 
 
 class ProductsForm(forms.ModelForm):
+    forbidden_list = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
 
     class Meta:
         model = Products
         fields = '__all__'
+
+    def clean_name(self):
+        cleaned_data = self.cleaned_data['name']
+
+        if cleaned_data in self.forbidden_list:
+            raise forms.ValidationError('Данный продукт находится в списке запрещенных')
+
+        return cleaned_data
+
+    def clean_description(self):
+        cleaned_data = self.cleaned_data['description']
+
+        if set([word.upper() for word in cleaned_data.split()]) & set([word.upper() for word in self.forbidden_list]):
+            raise forms.ValidationError('Данный продукт находится в списке запрещенных')
+
+        return cleaned_data
