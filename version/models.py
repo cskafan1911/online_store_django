ec611bfbbd595c1d2e9112e035210b79c46fa98d
Dmitry Dronov <cskafan1911@gmail.com>
2023-12-24 20:42:33 +0300

    Написана модель Version

--- version/models.py
+++ version/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Version(models.Model):
+    product = models.ForeignKey('catalog.Products', on_delete=models.CASCADE, verbose_name='Продукт')
+    version_number = models.AutoField(primary_key=True, verbose_name='Номер версии')
+    version_name = models.CharField(max_length=150, verbose_name='Название версии')
+    version_status = models.BooleanField(default=False, verbose_name='Статус версии')
+
+    def __str__(self):
+        return f'{self.version_number} - {self.version_name}: {self.version_status}'
+
+    class Meta:
+        verbose_name = 'Версия'
+        verbose_name_plural = 'Версии'
