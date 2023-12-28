53e3c2b225f81775b005d363b6373287df7e2036
Dmitry Dronov <cskafan1911@gmail.com>
2023-12-28 17:40:48 +0300

    Исправления в форме регистрации,

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 from users.models import User
 
@@ -7,4 +7,11 @@ class UserRegisterForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ('email', 'password1', 'password2')
+        fields = ('avatar', 'phone', 'country', 'email', 'password1', 'password2')
+
+
+class UserProfileForm(UserChangeForm):
+
+    class Meta:
+        model = User
+        fields = ('email', 'first_name', 'last_name', 'avatar', 'phone', 'country')
