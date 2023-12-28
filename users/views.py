4084c2cced40fb289d8e44b78e631c278f78dd79
Dmitry Dronov <cskafan1911@gmail.com>
2023-12-28 17:06:05 +0300

    Написан контроллер для регистрации, создан шаблон register.html

--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import CreateView
 
-# Create your views here.
+from users.models import User
+
+
+class RegisterView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    template_name = 'users/register.html'
