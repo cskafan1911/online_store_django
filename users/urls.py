3c63b6dbeb521bcb8699f65cdbee3ec98bdef132
Dmitry Dronov <cskafan1911@gmail.com>
2024-01-04 18:13:33 +0300

    В приложение users добавлена верификация почты пользователя при регистрации

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,8 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import RegisterView, ProfileView
+from users.views import RegisterView, ProfileView, UserConfirmEmailView, EmailConfirmationSentView, EmailConfirmedView, \
+    EmailConfirmationFailedView
 
 app_name = UsersConfig.name
 
@@ -10,5 +11,10 @@ urlpatterns = [
     path('', LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
-    path('profile/', ProfileView.as_view(), name='profile')
+    path('profile/', ProfileView.as_view(), name='profile'),
+    path('confirm-email/<str:uidb64>/<str:token>', UserConfirmEmailView.as_view(), name='confirm_email'),
+    path('email-confirmation-sent/', EmailConfirmationSentView.as_view(), name='email_confirmation_sent'),
+    path('email-confirmed/', EmailConfirmedView.as_view(), name='email_confirmed'),
+    path('email-confirmation-failed/', EmailConfirmationFailedView.as_view(), name='email_confirmation_failed'),
+
 ]
--- users/views.py
+++ users/views.py
@@ -1,9 +1,15 @@
 import random
 
+from django.contrib.auth import login
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.sites.shortcuts import get_current_site
 from django.core.mail import send_mail
 from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, UpdateView
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.views import View
+from django.views.generic import CreateView, UpdateView, TemplateView
 
 from config import settings
 from users.forms import UserRegisterForm, UserProfileForm
@@ -15,6 +21,66 @@ class RegisterView(CreateView):
     form_class = UserRegisterForm
     template_name = 'users/register.html'
     success_url = reverse_lazy('users:login')
+
+    def form_valid(self, form):
+        user = form.save(commit=False)
+        user.save()
+        token = default_token_generator.make_token(user)
+        uid = urlsafe_base64_encode(force_bytes(user.pk))
+        activation_url = reverse_lazy('users:confirm_email', kwargs={'uidb64':uid, 'token': token})
+        current_site = get_current_site(self.request)
+        send_mail(
+            'Подтвердите электронный адрес',
+            f'Ссылка для подтверждения: http://{current_site.domain}{activation_url}',
+            settings.EMAIL_HOST_USER,
+            [user.email]
+        )
+        return redirect('users:email_confirmation_sent')
+
+
+class UserConfirmEmailView(View):
+
+    @staticmethod
+    def get(request, uidb64, token):
+        try:
+            uid = urlsafe_base64_decode(uidb64)
+            user = User.objects.get(pk=uid)
+        except (TypeError, ValueError, OverflowError):
+            user = None
+
+        if user is not None and default_token_generator.check_token(user, token):
+            user.email_verify = True
+            user.save()
+            login(request, user)
+            return redirect('users:email_confirmed')
+        return redirect('users:email_confirmation_failed')
+
+
+class EmailConfirmationSentView(TemplateView):
+    template_name = 'users/email_confirmation_sent.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Письмо активации отправлено'
+        return context
+
+
+class EmailConfirmedView(TemplateView):
+    template_name = 'users/email_confirmed.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Ваша почта активирована'
+        return context
+
+
+class EmailConfirmationFailedView(TemplateView):
+    template_name = 'users/email_confirmation_failed.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Ваша почта не активирована'
+        return context
 
 
 class ProfileView(UpdateView):
