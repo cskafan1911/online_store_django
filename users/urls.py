f2ae7f90cf02d825959db42100858047a2728d2d
Dmitry Dronov <cskafan1911@gmail.com>
2024-01-04 19:35:40 +0300

    В приложение users добавлена возможность восстанавливать пароль по ссылке на почту.

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,8 @@
-from django.contrib.auth.views import LoginView, LogoutView
-from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView, PasswordResetDoneView, \
+    PasswordResetConfirmView, PasswordResetCompleteView
+from django.urls import path, reverse_lazy
 
+from config import settings
 from users.apps import UsersConfig
 from users.views import RegisterView, ProfileView, UserConfirmEmailView, EmailConfirmationSentView, EmailConfirmedView, \
     EmailConfirmationFailedView
@@ -16,5 +18,18 @@ urlpatterns = [
     path('email-confirmation-sent/', EmailConfirmationSentView.as_view(), name='email_confirmation_sent'),
     path('email-confirmed/', EmailConfirmedView.as_view(), name='email_confirmed'),
     path('email-confirmation-failed/', EmailConfirmationFailedView.as_view(), name='email_confirmation_failed'),
-
+    path('password-reset/', PasswordResetView.as_view(template_name='users/password_reset_form.html',
+                                                      email_template_name='users/password_reset_email.html',
+                                                      from_email=settings.EMAIL_HOST_USER,
+                                                      success_url=reverse_lazy('users:password_reset_done')),
+         name='password_reset'),
+    path('password-reset/done', PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
+         name='password_reset_done'),
+    path('password-reset/<uidb64>/<token>/',
+         PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html',
+                                          success_url=reverse_lazy('users:password_reset_complete')),
+         name='password_reset_confirm'),
+    path('password_reset/complete/',
+         PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
+         name='password_reset_complete')
 ]
